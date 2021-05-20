1246ccd2e96c7a0722cf323f88d89e09f788aee0
scarymovie <81684836+scarymovie@users.noreply.github.com>
2021-05-21 00:08:15 +0300

    add users profile v2

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,211 @@
+<x-app-layout>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>User page</title>
+
+        <!-- Google Font: Source Sans Pro -->
+        <link rel="stylesheet"
+              href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+        <!-- Font Awesome -->
+        <link rel="stylesheet" href="../../plugins/fontawesome-free/css/all.min.css">
+        <!-- Theme style -->
+        <link rel="stylesheet" href="../../dist/css/adminlte.min.css">
+    </head>
+    <body class="hold-transition sidebar-mini">
+
+
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1>Profile</h1>
+                    </div>
+                    <div class="col-sm-6">
+
+                    </div>
+                </div>
+            </div><!-- /.container-fluid -->
+        </section>
+
+        <!-- Main content -->
+        <section class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-3">
+
+                        <!-- Profile Image -->
+                        <div class="card card-primary card-outline">
+                            <div class="card-body box-profile">
+                                <div class="text-center">
+                                    <img class="profile-user-img img-fluid img-circle"
+                                         src="../../dist/img/user4-128x128.jpg"
+                                         alt="User profile picture">
+                                </div>
+
+                                {{$page->name}}
+
+                                <p class="text-muted text-center">Software Engineer</p>
+                            </div>
+                            <!-- /.card-body -->
+                        </div>
+                        <!-- /.card -->
+
+                        <!-- About Me Box -->
+                        <div class="card card-primary">
+                            <div class="card-header">
+                                <h3 class="card-title">About Me</h3>
+                            </div>
+                            <!-- /.card-header -->
+                            <div class="card-body">
+                                <strong><i class="fas fa-book mr-1"></i>Education</strong>
+
+                                <p class="text-muted">
+                                    {{$page->about}}
+                                </p>
+                                <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+                                    <div class="block mb-8">
+                                        <a href="#"
+                                           class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Add
+                                            about</a>
+                                    </div>
+                                    <tr>
+                                        <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">
+                                            <a href="#"class="text-indigo-600 hover:text-indigo-900 mb-2 mr-2">Edit</a>
+                                        </td>
+                                    </tr>
+                                    <x-jet-section-border/>
+                                </div>
+
+                                <strong><i class="fas fa-map-marker-alt mr-1"></i> Location</strong>
+
+                                <p class="text-muted">Malibu, California</p>
+
+                                <hr>
+
+                                <strong><i class="fas fa-pencil-alt mr-1"></i> Skills</strong>
+
+                                <p class="text-muted">
+                                    <span class="tag tag-danger">UI Design</span>
+                                    <span class="tag tag-success">Coding</span>
+                                    <span class="tag tag-info">Javascript</span>
+                                    <span class="tag tag-warning">PHP</span>
+                                    <span class="tag tag-primary">Node.js</span>
+                                </p>
+
+                                <hr>
+
+                                <strong><i class="far fa-file-alt mr-1"></i> Notes</strong>
+
+                                <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
+                                    fermentum enim neque.</p>
+                            </div>
+                            <!-- /.card-body -->
+                        </div>
+                        <!-- /.card -->
+                    </div>
+
+                    <div class="col-md-9">
+                        <div class="card">
+                            <div class="card-header p-2 col-md-12">
+                                <ul class="nav nav-pills">
+                                    <li class="nav-item"><a class="nav-link active" href="#activity" data-toggle="tab">Activity</a>
+                                    </li>
+                                </ul>
+                            </div><!-- /.card-header -->
+                            <div class="card-body">
+                                <div class="tab-content">
+                                    <div class="active tab-pane" id="activity">
+                                        <!-- Post -->
+                                        <div class="post">
+                                            <div class="user-block">
+                                                <span class="username">
+                                            </div>
+                                            <!-- /.user-block -->
+
+                                        </div>
+                                        <!-- /.post -->
+                                        <div class="post">
+                                            <div class="user-block">
+                                                <img class="img-circle img-bordered-sm"
+                                                     src="../../dist/img/user1-128x128.jpg" alt="user image">
+                                                <span class="username">
+                          {{--<a href="#">{{Auth::user()->name}}</a>--}}
+                        </span>
+                                                <span class="description">Shared publicly - 7:30 PM today</span>
+                                            </div>
+                                            <!-- /.user-block -->
+                                            <p>
+                                                Lorem ipsum represents a long-held tradition for designers,
+                                                typographers and the like. Some people hate it and argue for
+                                                its demise, but others ignore the hate as they create awesome
+                                                tools to help create filler text for everyone from bacon lovers
+                                                to Charlie Sheen fans.
+                                            </p>
+                                        </div>
+                                        <!-- /.user-block -->
+                                        @if($userId = $page->id)
+                                            @foreach($page->posts as $item)
+                                                <div class="post">
+                                                    <div class="user-block">
+                                                        <img class="img-circle img-bordered-sm"
+                                                             src="../../dist/img/user1-128x128.jpg" alt="user image">
+                                                        <span class="username">
+                          <a href="#">{{$page->name}}</a>
+                          <a href="#" class="float-right btn-tool"><i class="fas fa-times"></i></a>
+                        </span>
+                                                        <span class="description">Shared publicly - 7:30 PM today</span>
+                                                    </div>
+                                                    <ul>
+                                                        <li>{{$item->essence}}</li>
+                                                    </ul>
+                                                </div>
+                                        @endforeach
+                                    @endif
+                                    <!-- /.tab-content -->
+                                    </div><!-- /.card-body -->
+
+                                </div>
+
+                                <!-- /.card -->
+                            </div>
+
+                            <!-- /.col -->
+                        </div>
+
+
+                    </div>
+
+        </section>
+        <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+
+
+    <!-- Control Sidebar -->
+    <aside class="control-sidebar control-sidebar-dark">
+        <!-- Control sidebar content goes here -->
+    </aside>
+    <!-- /.control-sidebar -->
+    </div>
+    <!-- ./wrapper -->
+
+    <!-- jQuery -->
+    <script src="../../plugins/jquery/jquery.min.js"></script>
+    <!-- Bootstrap 4 -->
+    <script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="../../dist/js/adminlte.min.js"></script>
+    <!-- AdminLTE for demo purposes -->
+    <script src="../../dist/js/demo.js"></script>
+    </body>
+    </html>
+
+
+</x-app-layout>
+
