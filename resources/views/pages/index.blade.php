f5456733967c4a2964343b3b239a1576a46c5bb6
scarymovie <81684836+scarymovie@users.noreply.github.com>
2021-06-04 01:42:25 +0300

    good front and translate v2

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -21,13 +21,6 @@
     <div class="content-wrapper">
         <!-- Content Header (Page header) -->
         <section class="content-header">
-            <div class="container-fluid">
-                <div class="row mb-2">
-                    <div class="col-sm-6">
-                        <h1>Профиль</h1>
-                    </div>
-                </div>
-            </div><!-- /.container-fluid -->
         </section>
 
         <!-- Main content -->
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -22,16 +22,7 @@
     <div class="content-wrapper">
         <!-- Content Header (Page header) -->
         <section class="content-header">
-            <div class="container-fluid">
-                <div class="row mb-2">
-                    <div class="col-sm-6">
-                        <h1>Профиль</h1>
-                    </div>
-                    <div class="col-sm-6">
 
-                    </div>
-                </div>
-            </div><!-- /.container-fluid -->
         </section>
 
         <!-- Main content -->
