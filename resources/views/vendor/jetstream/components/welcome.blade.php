305ab4ee6cb548355514b65a0784c273ff7bbe91
scarymovie <81684836+scarymovie@users.noreply.github.com>
2021-07-19 18:45:02 +0300

    lorem v2

--- resources/views/vendor/jetstream/components/welcome.blade.php
+++ resources/views/vendor/jetstream/components/welcome.blade.php
@@ -99,7 +99,7 @@
                         <path
                             d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"></path>
                     </svg>
-                    <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold">ИСТ</div>
+                    <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold">Lorem</div>
                 </div>
 
                 <div class="ml-12">
